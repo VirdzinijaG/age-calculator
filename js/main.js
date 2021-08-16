3ffcae84780ee40f1a97fc276325acbf2b35593d
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-16 12:35:14 +0300

    js

---
+++ js/main.js
@@ -0,0 +1,63 @@
+const months = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+function ageCalculate(){
+    let today = new Date();
+    let inputDate = new Date(document.getElementById("input-date").value);
+    let birthMonth,birthDate,birthYear;
+    let birthDetails = {
+        date:inputDate.getDate(),
+        month:inputDate.getMonth()+1,
+        year:inputDate.getFullYear()
+    }
+    let currentYear = today.getFullYear();
+    let currentMonth = today.getMonth()+1;
+    let currentDate = today.getDate();
+
+
+    if(
+        birthDetails.year > currentYear ||
+        ( birthDetails.month > currentMonth && birthDetails.year == currentYear) || 
+        (birthDetails.date > currentDate && birthDetails.month == currentMonth && birthDetails.year == currentYear)
+    ){
+        alert("Dar negimė");
+        displayResult("-","-","-");
+        return;
+    }
+    if( birthDetails.month === currentMonth && birthDetails.year === currentYear && birthDetails.date === currentDate
+    ){
+        alert("Su gimtadieniu");
+        displayResult(birthDate,birthMonth,birthYear);
+        return;
+    }
+
+    birthYear = currentYear - birthDetails.year;
+
+    if(currentMonth >= birthDetails.month){
+        birthMonth = currentMonth - birthDetails.month;
+    }
+    else{
+        birthYear--;
+        birthMonth = 12 + currentMonth - birthDetails.month;
+    }
+
+    if(currentDate >= birthDetails.date){
+        birthDate = currentDate - birthDetails.date;
+    }
+    else{
+        birthMonth--;
+        let days = months[currentMonth - 2];
+        birthDate = days + currentDate - birthDetails.date;
+        if(birthMonth < 0){
+            birthMonth = 11;
+            birthYear--;
+        }
+    }
+    displayResult(birthDate,birthMonth,birthYear);
+}
+
+function displayResult(bDate,bMonth,bYear){
+    document.getElementById("years").textContent = bYear;
+    document.getElementById("months").textContent = bMonth;
+    document.getElementById("days").textContent = bDate;
+}
+
